c7a713120dd0b506a0c818720dcf3ec0bd192939
Vimuwa96 <114574555+Vimuwa96@users.noreply.github.com>
2023-01-02 15:34:40 +0530

    Update dark_mode.js

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("button").click(function () {
+    $(".dark_mode").click(function () {
         $(".dark_mode_change").toggleClass("dark_mode_on");
     });
 });
